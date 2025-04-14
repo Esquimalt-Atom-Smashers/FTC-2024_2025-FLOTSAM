32c0cd5f0a8298b2a7b9d1ce03112ffdaf0efec3
Esqatomsmashers-Students <esqatomsmashers+github.students@gmail.com>
2025-04-14 12:24:30 -0700

    In progress: limelight subsystem
    Working toward a function that gives RR coordinate of the robot on the field

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LimelightSubsystem.java
@@ -0,0 +1,115 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+import java.util.List;
+
+public class LimelightSubsystem extends SubsystemBase {
+    OpMode opMode;
+    Telemetry telemetry;
+
+    Limelight3A limelight;
+
+    public LimelightSubsystem(OpMode opMode) {
+        limelight = opMode.hardwareMap.get(Limelight3A.class, "limelight");
+        this.telemetry = opMode.telemetry;
+        this.telemetry.setMsTransmissionInterval(11);
+
+        limelight.pipelineSwitch(0);
+        limelight.start();
+    }
+
+    public double[] getLimelightDegree(Limelight3A limelight) {
+        LLResult result = limelight.getLatestResult();
+        double xDegree = 10000;
+        double yDegree = 10000;
+        if (result != null) {
+//            Pose3D botpose = result.getBotpose();
+            double captureLatency = result.getCaptureLatency();
+            double targetingLatency = result.getTargetingLatency();
+//            double parseLatency = result.getParseLatency();
+            telemetry.addData("LL Latency", captureLatency + targetingLatency);
+//            telemetry.addData("Parse Latency", parseLatency);
+//            telemetry.addData("PythonOutput", java.util.Arrays.toString(result.getPythonOutput()));
+
+            if (result.isValid()) {
+//                telemetry.addData("tx", result.getTx());
+//                telemetry.addData("txnc", result.getTxNC());
+//                telemetry.addData("ty", result.getTy());
+//                telemetry.addData("tync", result.getTyNC());
+//
+//                telemetry.addData("Botpose", botpose.toString());
+
+                // Access fiducial results
+                List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
+                for (LLResultTypes.FiducialResult fr : fiducialResults) {
+                    double xRes = fr.getTargetXDegrees();
+                    double yRes = fr.getTargetYDegrees();
+                    telemetry.addData("Fiducial", "ID: %d, Family: %s, X: %.2f, Y: %.2f", fr.getFiducialId(), fr.getFamily(),xRes, yRes);
+
+                    if(Math.abs(xRes) < Math.abs(xDegree) && Math.abs(yRes) < Math.abs(yDegree)) {
+                        xDegree = xRes;
+                        yDegree = yRes;
+                    }
+                }
+
+                // Access color results
+                List<LLResultTypes.ColorResult> colorResults = result.getColorResults();
+                for (LLResultTypes.ColorResult cr : colorResults) {
+                    double xRes = cr.getTargetXDegrees();
+                    double yRes = cr.getTargetYDegrees();
+                    telemetry.addData("Color", "X: %.2f, Y: %.2f", cr.getTargetXDegrees(), cr.getTargetYDegrees());
+                    if(Math.abs(xRes) < Math.abs(xDegree) && Math.abs(yRes) < Math.abs(yDegree)) {
+                        xDegree = xRes;
+                        yDegree = yRes;
+                    }
+                }
+            }
+        } else {
+            telemetry.addData("Limelight", "No data available");
+        }
+
+        return new double[] {-xDegree, yDegree};
+    }
+
+    public double[] getRobotPoseOnField() {
+        LLResult result = limelight.getLatestResult();
+        double X = 10000;
+        double Y = 10000;
+        if (result != null) {
+            Pose3D botpose = result.getBotpose();
+            double captureLatency = result.getCaptureLatency();
+            double targetingLatency = result.getTargetingLatency();
+//            double parseLatency = result.getParseLatency();
+            telemetry.addData("LL Latency", captureLatency + targetingLatency);
+//            telemetry.addData("Parse Latency", parseLatency);
+//            telemetry.addData("PythonOutput", java.util.Arrays.toString(result.getPythonOutput()));
+
+            if (result.isValid()) {
+//                telemetry.addData("tx", result.getTx());
+//                telemetry.addData("txnc", result.getTxNC());
+//                telemetry.addData("ty", result.getTy());
+//                telemetry.addData("tync", result.getTyNC());
+                X = botpose.getPosition().x;
+
+                telemetry.addData("Botpose", botpose.toString());
+
+                // Access fiducial results
+                List<LLResultTypes.FiducialResult> fiducialResults = result.getFiducialResults();
+                for (LLResultTypes.FiducialResult fr : fiducialResults) {
+                    telemetry.addData("Fiducial", "ID: %d, Family: %s, X: %.2f, Y: %.2f", fr.getFiducialId(), fr.getFamily(),xRes, yRes);
+                }
+            }
+        } else {
+            telemetry.addData("Limelight", "No data available");
+        }
+        return new double[] {X, Y};
+    }
+}
