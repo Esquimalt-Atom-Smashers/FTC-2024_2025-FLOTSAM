f65018a735cc5077fc179d6d5e6ad10f4d21ddaa
Esqatomsmashers-Students <esqatomsmashers+github.students@gmail.com>
2025-06-05 17:15:35 -0700

    Webcam detect blue + tell degree

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/zoldprograms/WebcamColourOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/zoldprograms/WebcamColourOpMode.java
@@ -1,29 +1,7 @@
-/*
- * Copyright (c) 2024 Phil Malone
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package org.firstinspires.ftc.teamcode.opmodes.zoldprograms;
 
 import android.util.Size;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -37,7 +15,7 @@ import org.opencv.core.RotatedRect;
 
 import java.util.List;
 
-@TeleOp(name = "WebcamColourDetect", group = "zoldprograms")
+@TeleOp(name = "WebcamDetectColour", group = "zoldprograms")
 public class WebcamColourOpMode extends LinearOpMode
 {
     @Override
@@ -46,7 +24,7 @@ public class WebcamColourOpMode extends LinearOpMode
         ColorBlobLocatorProcessor colorLocator = new ColorBlobLocatorProcessor.Builder()
                 .setTargetColorRange(ColorRange.BLUE)         // use a predefined color match
                 .setContourMode(ColorBlobLocatorProcessor.ContourMode.EXTERNAL_ONLY)    // exclude blobs inside blobs
-                .setRoi(ImageRegion.asUnityCenterCoordinates(-0.5, 0.5, 0.5, -0.5))  // search central 1/4 of camera view
+                .setRoi(ImageRegion.asUnityCenterCoordinates(-1, 1, 1, 0))  // search central 1/4 of camera view
                 .setDrawContours(true)                        // Show contours on the Stream Preview
                 .setBlurSize(5)                               // Smooth the transitions between different colors in image
                 .build();
@@ -65,14 +43,16 @@ public class WebcamColourOpMode extends LinearOpMode
             List<ColorBlobLocatorProcessor.Blob> blobs = colorLocator.getBlobs();
             ColorBlobLocatorProcessor.Util.filterByArea(50, 20000, blobs);  // filter out very small blobs.
 
-            telemetry.addLine(" Area Density Aspect  Center");
+            telemetry.addLine(" Area Density Aspect  Center   degree");
 
             // Display the size (area) and center location for each Blob.
             for(ColorBlobLocatorProcessor.Blob b : blobs)
             {
                 RotatedRect boxFit = b.getBoxFit();
+                double degree = boxFit.center.x / 318 * 68.5 - (68.5 / 2);
                 telemetry.addLine(String.format("%5d  %4.2f   %5.2f  (%3d,%3d)",
                         b.getContourArea(), b.getDensity(), b.getAspectRatio(), (int) boxFit.center.x, (int) boxFit.center.y));
+                telemetry.addData("degree", degree);
             }
 
             telemetry.update();
