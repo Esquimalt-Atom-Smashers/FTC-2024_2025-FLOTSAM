a510c337c05013a0c35b574b24a10a192e6f9e4e
BoscoMaker <bco445776@gmail.com>
2025-06-08 21:07:14 -0700

    To be tested: little patches on SampleAIMING

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/zoldprograms/WebcamColourOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/zoldprograms/WebcamColourOpMode.java
@@ -55,6 +55,7 @@ public class WebcamColourOpMode  extends LinearOpMode
                 telemetry.addLine(String.format("%5d  %4.2f   %5.2f  (%3d,%3d)",
                         b.getContourArea(), b.getDensity(), b.getAspectRatio(), (int) boxFit.center.x, (int) boxFit.center.y));
                 telemetry.addData("Webcam degree", degree);
+                telemetry.addData("b angle", boxFit.angle);
             }
 
             double[] sampleRes = limelightSubsystem.getBlueSample();
